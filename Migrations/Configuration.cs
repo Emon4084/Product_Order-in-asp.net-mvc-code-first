dd7bbcbc328cae4ef2b63f89d13d3998d09c0c2b
Emon4084 <jahidulemon49@gmail.com>
2023-10-08 21:55:03 +0600

    Add project files.

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,26 @@
+﻿namespace My_Products.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<My_Products.DAL.OrderContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+
+        }
+    
+
+        protected override void Seed(My_Products.DAL.OrderContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
